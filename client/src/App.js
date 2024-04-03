2a37e6e3e60828f76ae8a6a8450157312c574774
Marilia Kokkinou <marilia.kokkinou@gfk.com>
2024-04-03 14:49:34 +0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator
+            Fib calculator version 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
